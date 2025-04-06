3e669fc800b7b5d5fbf726282d8339d1b1ed9059
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-04-06 20:13:13 +0000

    list

---
+++ Day-1.py
@@ -0,0 +1,17 @@
+import os
+
+folders = input("Emter the names of folders separated by space").split()
+for folder in folders:
+  
+  try:
+    files = os.listdir(folder)
+
+  except FileNotFoundError:
+     print ("Provide valid dir name")
+     continue
+  
+  print("=========Listing the files for folder========" + folder)
+
+  for file in files:
+     print(file)
+  
