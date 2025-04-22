02ec31ebb4e99c894202fa40fb368e5cc3c8cfbe
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-04-22 17:29:06 +0000

    modified

--- Automate-Log-File.py
+++ Automate-Log-File.py
@@ -1,7 +1,7 @@
-
-
-
-
+""" This script will:
+- Check the size of the log file.
+- If it exceeds a specified limit (e.g., 5 MB), it will rename the current log file and create a new one.
+- Keep a specified number of backup logs (e.g., 5). """
 
 import os
 import gzip
@@ -9,4 +9,16 @@ import shutil
 from datetime import datetime
 
 #Configuration
-MaxSize = 5
+MAX_LOG_SIZE = 5
+MAX_BACKUPS = 5
+LogFile = "app.log"
+
+
+
+
+
+
+
+
+
+
