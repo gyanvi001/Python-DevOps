cd15d209f063bda2e8f44c18116a850271038975
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-04-25 17:57:37 +0000

    Updated

---
+++ Check-Linux-Process.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def is_service_running(servicename):
+    status = subprocess.run([ 'systemctl' , 'isactive' , servicename], stdout = subprocess.PIPE)
+    return status.stdout.decode().strip() == "active"
+
+service = "ngnix"
+if is_service_running(service):
+    print(f"{service} is running ")
+else:
+    print(f"{service} is not running")
