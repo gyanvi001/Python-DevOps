35e9c37dd02e37a5d493ba180dfcf46fc31eb1a2
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-04-27 17:23:00 +0000

    Initial

---
+++ BackupFolder.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+from datetime import datetime
+
+
+#Source directory
+src = "c:/D/Documents"
+
+#Destination directory
+des = "C:/Devops/Projects"
+
+#Create a timestamp folder for backup
+timestamp = datetime.now().strftime('%Y%M%D_%H%M%S')
+backup_folder = os.path.join(des, f'backup_{timestamp}')
+
+#Create backup of the folder
+try:
+    shutil.copytree(src,des)
+    print ("Back up completed at {backup_folder}")
+except Exception as e:
+    print("Error during backing {e}")
+
+
+
