9f60a680e4b3d56f1380004d9b34b04e66ccf419
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-05-03 16:58:02 +0000

    modify

--- JiraProjects.py
+++ JiraProjects.py
@@ -1,3 +1,31 @@
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
 
+url = "https://gyanvi-pandey.atlassian.net/rest/api/3/project"
 
+#api_key = os.getenv("JIRA_TOKEN")
+#print(api_key)
 
+and96B"
+
+auth = HTTPBasicAuth("gyanvipandey1999@gmail.com", api_key)
+
+headers = {
+  "Accept": "application/json"
+}
+
+response = requests.request(
+   "GET",
+   url,
+
+   headers=headers,
+   auth=auth
+)
+
+output = json.loads(response.text)
+name = output[1]["name"]
+print(name)
