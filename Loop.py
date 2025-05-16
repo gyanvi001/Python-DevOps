5a76a01a3a9a1f5f1d7245566343fb22c405d1c0
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-05-16 00:50:06 +0000

    Modified

--- Loop.py
+++ Loop.py
@@ -1,3 +1,19 @@
 
 for i in range(1,10):
     print("number", i)
+
+
+    num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(-7)
+print("Absolute Value:", result3)
